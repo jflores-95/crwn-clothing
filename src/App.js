b474b89529d76335bee079050091f2f5ba805a69
jflores-95 <66271850+jflores-95@users.noreply.github.com>
2022-03-10 17:54:32 -0600

    Adding react router Directory with urls HOC withRouter to Menuitems

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from "react";
 
 import "./App.css";
-import HomePage from "./homepage.component";
-import "./homepage.styles.scss";
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage.styles.scss";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats</h1>
+    </div>
+  );
+};
+
+const TopicList = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topics</h1>
+    </div>
+  );
+};
+
+const TopicDetail = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic detail {`${props.match.params.topicId}`}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/topics" component={TopicList} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => {
+  
+    return (
+        <div>
+            <div className="homepage">
+               <Directory />
+                </div>
+        </div>
+    );
+}
+
+export default HomePage;
