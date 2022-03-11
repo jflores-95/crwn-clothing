da1a290a8d157021ad0cef5af721d01cc7101c6d
jflores-95 <66271850+jflores-95@users.noreply.github.com>
2022-03-10 20:35:45 -0600

    Setting up Shop and collection preview component

--- src/App.js
+++ src/App.js
@@ -4,40 +4,14 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import "./pages/homepage/homepage.styles.scss";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats</h1>
-    </div>
-  );
-};
-
-const TopicList = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Topics</h1>
-    </div>
-  );
-};
-
-const TopicDetail = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Topic detail {`${props.match.params.topicId}`}</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/topics" component={TopicList} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
